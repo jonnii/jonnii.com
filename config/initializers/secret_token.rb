8d6e7043db098d387bae64d43da0d01bfe303b4c
Jonathan Goldman <me@jonnii.com>
2011-06-23 20:37:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jonnii::Application.config.secret_token = '31df228895fbcc7e5718fbb61136f2114a966f9f72d11915a7b28d9074cf5640c7ece80a1c28f75f2202191737d5cc057ac92ddf63c2a447e12081b3233d43d1'
